c459b891b3f9f01c5bb81cf22bf70e9a592a6b0a
Ratmir <ratmir.yuldashev28@mail.ru>
2021-05-03 01:52:25 +0600

    fix4

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../components/css/Detail.css'
+// import '../components/css/Detail.css'
 // import { useParams } from 'react-router-dom';
 
 const Detail = ({news}) => {
