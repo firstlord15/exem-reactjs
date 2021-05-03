d5ecfa4dc57eaad47190e23e0b12c999fd3b11a2
Ratmir <ratmir.yuldashev28@mail.ru>
2021-05-03 17:35:53 +0600

    fix-img finish

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './css/Card.css'
 
 const Cards = ({ news }) => {
     return (
         <>
             {news.map((cards, index) => (
-                <div key={index} className="col-md-3 opca mt-2">
+                <div key={index} className="col-md-3 col-sm-6 opca mt-2">
                     <div key={index} className="card bg-dark text-center news-item">
-                        <img className="card-img-top" alt="" src={cards.image} />
+                        <img className="card-img-top img-card-min" alt="" src={cards.image} />
                         <div className="card-body">
                             <h3 className="card-title">{cards.short_title}</h3>
                             <p className="card-text">{cards.shor_content}</p>
                         </div>
                         <Link className="m-4 btn btn-game" to={`/items/${cards.id}`}>
                             In Detail
-                    </Link>
+                        </Link>
                     </div>
                 </div>
             ))}
