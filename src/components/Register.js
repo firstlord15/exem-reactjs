4257d78f8945f0de72693b431d50140ef8e36955
Firstlord15 <ratmir.yuldashev28@mail.ru>
2021-05-03 16:07:25 +0600

    small fix2 finish

--- src/components/Register.js
+++ src/components/Register.js
@@ -8,12 +8,12 @@ const Register = () => {
         <div className="container jumbotron div-modern2 shadow-sm mt-5">
             <div className="">
                 <form>
-                    <div class="form-group col-6">
+                    <div class="form-group col-md-6">
                         <label for="exampleInputEmail1">Email address</label>
                         <input type="email" required class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
                         <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                     </div>
-                    <div class="form-group col-6">
+                    <div class="form-group col-md-6">
                         <label for="exampleInputPassword1">Password</label>
                         <input type="password" required class="form-control" id="exampleInputPassword1" placeholder="Password"></input>
                     </div>
